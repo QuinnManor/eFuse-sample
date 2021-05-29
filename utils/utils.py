71b8a0b5be3c70dde4195ae1c92591acb94d53d4
QuinnManor <matthewq1428@gmail.com>
2021-05-29 10:56:14 -0400

    more universal get_path function

--- utils/utils.py
+++ utils/utils.py
@@ -5,4 +5,4 @@ from pathlib import Path
 
 repo_root = Path(git.Repo(os.getcwd(), search_parent_directories=True).git.rev_parse("--show-toplevel"))
 def get_path(x, repo_root=repo_root):
-    return f"{repo_root}/data/raw/{x}"
+    return f"{repo_root}/data/{x}"
